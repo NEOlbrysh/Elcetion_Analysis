34ad49f6f428515d0ee4c8c2d8a6236084110dd9
NEOlbrysh <103701561+NEOlbrysh@users.noreply.github.com>
2022-05-22 15:35:50 -0400

    Create PyPoll_Challenge.py

---
+++ PyPoll_Challenge.py
@@ -0,0 +1,6 @@
+![2022-05-22 (7)](https://user-images.githubusercontent.com/103701561/169712769-ed32848a-5b30-4516-8995-2cd736a97460.png)
+![2022-05-22 (2)](https://user-images.githubusercontent.com/103701561/169712781-3313b870-bc6b-4151-b54a-7ea26bbfa185.png)
+![2022-05-22 (12)](https://user-images.githubusercontent.com/103701561/169712784-a62cc110-317b-4e2e-a5af-bced5986c425.png)
+![2022-05-22 (10)](https://user-images.githubusercontent.com/103701561/169712788-02efe04e-1627-4b35-bb51-efda3b16310b.png)
+![2022-05-22 (9)](https://user-images.githubusercontent.com/103701561/169712789-557a7bdc-2ca3-483f-92b7-1b3bae0de022.png)
+![2022-05-22 (8)](https://user-images.githubusercontent.com/103701561/169712792-2612ac5b-ed2f-4e91-8a90-55449d8df843.png)
